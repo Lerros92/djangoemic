7ff0ac145d8660536538ec0d995e4ca3af89b897
Your Name <you@example.com>
2020-08-04 23:15:37 +0700

    Create models

--- baohanh/admin.py
+++ baohanh/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import receiveTable, item
+
+admin.site.register(receiveTable)
+admin.site.register(item)
--- baohanh/models.py
+++ baohanh/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from datetime import datetime, timedelta
 # Create your models here.
+
+class receiveTable(models.Model):
+    noteNumber = models.CharField(max_length=10, unique=True)
+    customers = models.CharField(max_length=50)
+    receiveDay = models.DateField(auto_now_add=True)
+    note = models.TextField()
+
+class item(models.Model):
+    noteNumber = models.ForeignKey(receiveTable, on_delete=models.CASCADE, related_name="item")
+    itemName = models.CharField(max_length=60)
+    quantity = models.IntegerField(max_length=6, null=False)
+    itemGroup = models.CharField(max_length=10)
+    status = models.CharField(max_length=128, null=True)
+    check = models.CharField(max_length=128, null=True)
+    conclude = models.CharField(max_length=128, null=True)
+    deadline = models.DateField(null=False, default=(datetime.now() + timedelta(days=2)).date())
+    note = models.TextField()
